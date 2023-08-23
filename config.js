623c79d3df4f5cec42465a8a0159e797c1f12574
Vihanga Ashinshana <96816258+vihangayt0@users.noreply.github.com>
2023-08-23 21:14:47 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || 'youre session id',
+POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'youre POSTGRESQL url',
+LANG: process.env.LANG || 'EN' ,
+ANTI_BAD: process.env.ANTI_BAD || 'false',
+MAX_SIZE: process.env.MAX_SIZE || 100,
+ONLY_GROUP: process.env.ONLY_GROUP || 'false',
+ANTI_LINK: process.env.ANTI_LINK || 'false' ,
+ANTI_BOT: process.env.ANTI_BOT || 'false',
+ALIVE: process.env.ALIVE || `default`,
+FOOTER: process.env.FOOTER ||  '©𝚉𝙴𝚁𝙾-𝚃𝚆𝙾 𝙼𝙳',
+LOGO: process.env.LOGO || `https://github.com/vihangayt0/ZeroTwo-Uploads/blob/main/ZERO-TWO.png?raw=true` 
+};
